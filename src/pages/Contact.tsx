09ecef37e14012d29ce9eae5b6b79b7f498cc3cd
KendiAstro23 <litalakendy975@gmail.com>
2025-07-01 14:33:44 +0300

    Implemented EmailJs functionality for receiving emails

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -29,7 +29,7 @@ const Contact = () => {
         SERVICE_ID,
         TEMPLATE_ID,
         {
-          to_email: 'litalakendi975@gmail.com',
+          to_email: 'litalakendy975@gmail.com',
           from_name: formData.name,
           from_email: formData.email,
           message: formData.message,
